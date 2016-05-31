4c392c662f7cdf6196d0b6e625899dd393d43b00
Sergey Goliney <sergey@goliney.com>
2016-05-31 20:58:17 +0300

    chore(releasing): Add travis config, conventional commit, semantic-release and dummy-test
    closes #1

---
+++ test/room.class.spec.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var expect = require('chai').expect;
+const Room = require('../lib/room.class.js');
+
+describe('Room class', function() {
+    it('should work!', function() {
+        expect(true).to.be.true;
+    });
+});
